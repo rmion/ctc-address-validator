26cdd3d52fdbde981bca5880a06c33f469b00df0
Robert Mion <mr.robert.mion@gmail.com>
2020-03-20 15:51:59 -0400

    Fixed data structure

--- functions/test.js
+++ functions/test.js
@@ -25,7 +25,7 @@ function isPointInsidePolygon(point, poly) {
 
 exports.handler = function(event, context, callback) {
     var resultList = []
-    var coords = event.queryStringParameters.lat + event.queryStringParameters.lng
+    var coords = { lat: event.queryStringParameters.lat, lng: event.queryStringParameters.lng }
     coordMap.forEach(function(poly) {
         resultList.push(
             isPointInsidePolygon(
